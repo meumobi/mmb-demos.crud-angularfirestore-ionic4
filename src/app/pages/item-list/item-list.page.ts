5c59d2ef6d56884edba5ccff507f5960b83784e7
vdias38 <victor.dias@meumobi.com>
2019-10-13 03:44:46 +0100

    feat: add skeleton on items-list loading

--- src/app/pages/item-list/item-list.page.ts
+++ src/app/pages/item-list/item-list.page.ts
@@ -3,6 +3,7 @@ import { Router } from '@angular/router';
 import { Item } from '../../shared/item.model';
 import { ItemService } from '../../shared/item.service';
 import { Observable } from 'rxjs';
+import { publishReplay, refCount } from 'rxjs/operators';
 
 @Component({
   selector: 'app-item-list',
@@ -19,7 +20,7 @@ export class ItemListPage implements OnInit {
   ) { }
 
   ngOnInit() {
-    this.items$ = this.itemsService.items$;
+    this.items$ = this.itemsService.items$.pipe(publishReplay(1), refCount());
   }
 
   deleteItem(id: string) {
