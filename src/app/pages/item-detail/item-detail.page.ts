f23a8055e1605650b658bea18a0ecc610cc324a5
vdias38 <victor.dias@meumobi.com>
2019-10-17 02:14:10 +0100

    feat(item-detail): add action sheet to update/delete - also replace toPromise on observable data service to observable in order to consider updates

--- src/app/pages/item-detail/item-detail.page.ts
+++ src/app/pages/item-detail/item-detail.page.ts
@@ -1,7 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Item } from 'src/app/shared/item.model';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { ItemService } from '../../shared/item.service';
+import { AlertController, ActionSheetController } from '@ionic/angular';
+import { Subscription } from 'rxjs';
 import { first } from 'rxjs/operators';
 
 @Component({
@@ -9,23 +11,94 @@ import { first } from 'rxjs/operators';
   templateUrl: './item-detail.page.html',
   styleUrls: ['./item-detail.page.scss'],
 })
-export class ItemDetailPage implements OnInit {
+export class ItemDetailPage implements OnInit, OnDestroy {
 
-  item: Item = null;
+  item: Item;
+  private sub: Subscription;
   private itemId: string;
 
   constructor(
     private activatedRoute: ActivatedRoute,
-    public itemService: ItemService
+    public itemService: ItemService,
+    public alertController: AlertController,
+    public actionSheetController: ActionSheetController,
+    private router: Router,
   ) { }
 
   ngOnInit() {
+    console.log('ItemDetailPage: OnInit');
     this.itemId = this.activatedRoute.snapshot.paramMap.get('id');
 
-    this.itemService.getById(this.itemId).pipe(first()).toPromise().then(
-      data => {
-        this.item = data;
-      }
-    );
+    this.sub = this.itemService.getById(this.itemId).subscribe(data => {
+      this.item = data;
+    });
+  }
+
+  ngOnDestroy() {
+    console.log('ItemDetailPage: OnDestroy');
+    this.sub.unsubscribe();
+  }
+
+  async presentActionSheet(event) {
+    event.preventDefault();
+    event.stopPropagation();
+    const actionSheet = await this.actionSheetController.create({
+      header: 'Confirm',
+      buttons: [{
+        text: 'Delete',
+        role: 'destructive',
+        icon: 'trash',
+        handler: () => {
+          this.deleteItem(this.itemId);
+        }
+      }, {
+        text: 'Update',
+        icon: 'create',
+        handler: () => {
+          this.updateItem(this.itemId);
+        }
+      }, {
+        text: 'Cancel',
+        icon: 'close',
+        role: 'cancel',
+        handler: () => {
+          console.log('Cancel clicked');
+        }
+      }]
+    });
+    await actionSheet.present();
+  }
+
+  async deleteItem(id: string) {
+    const alert = await this.alertController.create({
+      header: 'Confirm',
+      message: 'Are you sure you want to delete?',
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          cssClass: 'secondary',
+          handler: () => {
+            console.log('Cancel deletion');
+          }
+        }, {
+          text: 'Yes',
+          handler: () => {
+            this.itemService.remove(id);
+            this.navigateToItemsList();
+          }
+        }
+      ]
+    });
+
+    await alert.present();
+  }
+
+  updateItem(id: string) {
+    this.router.navigate([`/item-edit/${id}`]);
+  }
+
+  navigateToItemsList() {
+    this.router.navigate(['/item-list']);
   }
 }
